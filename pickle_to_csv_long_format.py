abc0019dc7d5f830791124195e795606a15e3df1
Zahra Arjmandi Lari <z.arjmandi@gmail.com>
2023-06-28 09:01:52 +0330

    prepared data for analysis in R. A long format csv; the players without ELO were removed

--- pickle_to_csv_long_format.py
+++ pickle_to_csv_long_format.py
@@ -7,19 +7,50 @@ with open("data_with_cent_loss.pkl", "rb") as f:
 df = pd.DataFrame(object)
 df.reset_index(drop=True, inplace=True)
 df_long = pd.DataFrame(columns=
-                       ['date',
-                        'name',
+                       ['game_id',
+                        'date',
                         'ELO',
+                        'name',
                         'color',
                         'if_won',
                         'count_moves',
                         'move_no',
                         'CP_loss'
                         ])
-
-for index, game in df.iterrows():
+current_row_idx = 0
+start_row_idx = 0
+for game_idx, game in df.iterrows():
     for idx_player, player in enumerate(['White', 'Black']):
+        start_row_idx = current_row_idx
+        # check if ELO is available for the player
+        ELO = game[player + ' ELO']
+        if ELO == 'NA':
+            continue
         cp_list = game[player + ' CP Loss List']
-        if_won = game['Result'][idx_player * 2]
-
-# df.to_csv('data_with_cent_loss.csv')
+        # check if there was not a tie
+        if len(game['Result']) == 3:
+            if_won = game['Result'][idx_player * 2]
+        else:
+            # if tie
+            if_won = '2'
+        count_moves = len(cp_list)
+        for move_no, cp in enumerate(cp_list):
+            df_long.loc[current_row_idx, ['move_no', 'CP_loss']] = \
+                move_no + 1, cp
+            current_row_idx += 1
+        df_long.loc[start_row_idx : start_row_idx + count_moves,
+                    ['game_id',
+                        'date',
+                        'ELO',
+                        'name',
+                        'color',
+                        'if_won',
+                        'count_moves']] = \
+                        (game_idx,
+                        game['Date'],
+                        ELO,
+                        game[player + ' Name'],
+                        player,
+                        if_won,
+                        count_moves)
+df_long.to_csv('data_with_cent_loss.csv')
